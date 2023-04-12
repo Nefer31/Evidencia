e25cca53ee0066a2975b81e35492bcee768bede1
Nefer Sneyder <nefersneyder506@gmail.com>
2023-04-11 22:19:06 -0500

    Co-authored-by: zippergamer07 <zippergamer07@users.noreply.github.com> Co-authored-by: nagoca01 <nagoca01@users.noreply.github.com>

--- paginaCompra/script.js
+++
@@ -1,6 +0,0 @@
-const comprarBtn = document.getElementById('comprar');
-const mensajeDiv = document.querySelector('.message');
-
-comprarBtn.addEventListener('click', () => {
-mensajeDiv.classList.remove('hidden');
-});
